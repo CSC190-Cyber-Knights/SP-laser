acdd4dad736e92c1a0450ce79a88ebfa29ec3bbd
SwagMaster87! <jamesayers00@gmail.com>
2023-11-26 18:10:56 -0800

    sign in page with google sign and a sign out btn

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
 import {ContactForm} from "./pages/ContactForm.jsx";
+import {SignIn} from "./pages/SignIn.jsx";
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path={'/'} element={<HomePage/>}/>
           <Route path={'/contact'} element={<ContactForm/>}/>
+          <Route path={"/signin"} element={<SignIn/>}/>
         </Routes>
       </BrowserRouter>
   )
---
+++ src/pages/SignIn.jsx
@@ -0,0 +1,34 @@
+import {GoogleButton} from 'react-google-button';
+import React from 'react';
+import {userSignIn, userSignOut} from "../firebaseConfig"
+
+export const SignIn = () => {
+    return (
+      <div>
+        <h1 className='text-center text-3xl font-bold py-8'>This is going to be the Sign In Page</h1>
+        <GoogleButton onClick={() => {
+            userSignIn()
+            }} />
+
+            <button onClick={() => {
+              userSignOut()
+            }}>
+              This is a button to sign out
+            </button>
+
+      </div>
+      
+      
+    );
+
+    
+  };
+/*
+//const signInButton = document.getElementById("signInButton")
+//const signOutButton = document.getElementById("signOutButton")
+
+
+signInButton.addEventListener('click', userSignIn)
+signOutButton.addEventListener('click',userSignOut)
+/*/
+export default SignIn;
