4b5674e4d874fe714f40afbe609ca754766f3100
Jacob Eberhardt <jacobeep26@yahoo.com>
2023-12-01 19:35:25 -0800

    added basic video player, requires video when executed so commented out

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,10 +1,29 @@
+//import url from '../assets/SampleVideo_1280x720_1mb.mp4'
 const GalleryPage = () => {
-  return (
-      <div>
-          <h1 className='text-center text-3xl font-bold py-8'>This is going to be the Photo Gallery Page</h1>
-      </div>
+    return (
+        <div>
+            <h1 className='text-center text-3xl font-bold py-8'>This is going to be the Photo Gallery Page</h1>
+            {/*<video controls autoPlay={true} muted={true}>*/}
+            {/*  <source src={url} type="video/mp4"/>*/}
+            {/*  <p>*/}
+            {/*      <a href="../assets/SampleVideo_1280x720_1mb.mp4"> link</a>*/}
+            {/*  </p>*/}
+            {/*</video>*/}
+        </div>
 
-  );
+    );
 
-};
+}
 export default GalleryPage;
+// class Video extends Component{
+//     render(){
+//         return (
+//             <div className="Video">
+//
+//                 <video controls autoPlay={true} muted={true}>
+//                     <source src={video} type="video/mp4"/>
+//                 </video>
+//             </div>
+//         );
+//     }
+// }
