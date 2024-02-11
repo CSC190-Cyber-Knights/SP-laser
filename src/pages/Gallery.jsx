a8dd67599452b11eacbf348122bc784aa82c9289
Jacob <114043730+J0831@users.noreply.github.com>
2024-02-11 11:12:19 -0800

    Setup gallery to separate items by category

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -2,6 +2,15 @@ const GalleryPage = () => {
   return (
       <div>
         <h1 className='text-center text-3xl font-bold py-8'>This is going to be the Photo Gallery Page</h1>
+          <div>
+              <h1 className='text-center text-3xl font-bold py-8'>Firearms</h1>
+          </div>
+          <div>
+              <h1 className='text-center text-3xl font-bold py-8'>Thermos</h1>
+          </div>
+          <div>
+              <h1 className='text-center text-3xl font-bold py-8'>Fishing Lures</h1>
+          </div>
       </div>
   );
 };
