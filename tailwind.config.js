961574faed060fb14bfd9448718f1e17104c881b
chris <actuallychrismendez@gmail.com>
2024-03-24 18:22:14 -0700

    edited tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,31 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
+    colors: {
+      red: 'FF0035',
+      grey: '8B939C',
+      violet: '59546C',
+      blue: '38405F',
+      black: '0E131F',
+    },
     extend: {
       backgroundImage: {
         'random-line': "url('./src/assets/line.svg')",
       },
       fontFamily: {
-        inter: ['Inter', 'sans-serif'],
+        sans: ['Inter', 'sans-serif'],
         poppins: ['Poppins', 'sans-serif'],
       },
+    },
+    borderRadius: {
+      '4xl': '2rem',
     },
   },
   plugins: [],
