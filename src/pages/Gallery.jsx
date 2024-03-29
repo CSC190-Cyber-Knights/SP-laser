c1efedd952386b8557452e62042a5165899413ce
DylanLacy <dylanlacy35@outlook.com>
2024-03-29 04:48:23 -0700

    Added ability to upload photos to firebase subfolders

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,7 +1,8 @@
 import {getStorage, ref, getDownloadURL, list, listAll, uploadBytesResumable} from 'firebase/storage'
 import {db} from './ContactForm.jsx'
 import * as url from 'url'
-import PhotoObj from "../components/ui/Photo.jsx";
+import PhotoObj from '../components/ui/Photo.jsx'
+import {useState} from 'react'
 
 //const docRef = doc(db, "gs://laserengraving-9a35a.appspot.com/images", "/IMG_0950.jpeg");
 //const docSnap = await getDoc(docRef);
@@ -9,66 +10,37 @@ const storage = getStorage() //connect to firebase storage
 const imagesRef = ref(storage, 'images/') //reference to image in firebase storage
 const testImage = ref(storage, 'images/IMG_0950.jpeg') //test image
 
-window.onload = function GeneratePhotos(){
-  console.log("GeneratePhotos Called")
+window.onload = function GeneratePhotos() {
+  console.log('GeneratePhotos Called')
   listAll(imagesRef)
-      .then((imagesListResult) => {
-        imagesListResult.prefixes.forEach((folderRef) => {
-          const category = folderRef.name + " w-1/6 h-1/6 photo"
-          CreateInput(folderRef.name)
+    .then((imagesListResult) => {
+      imagesListResult.prefixes.forEach((folderRef) => {
+        const category = folderRef.name + ' w-1/6 h-1/6 photo'
+        CreateInput(folderRef.name)
 
-          listAll(folderRef)
-              .then((itemListResult) => {
-                itemListResult.items.forEach((itemRef) => {
-                  getDownloadURL(ref(storage, itemRef.fullPath))
-                      .then((myUrl) => {
-                        console.log(category + ", " + itemRef.fullPath)
-                        CreateNewPhoto(category, myUrl)
-                      })
-                      .catch((error) => {})
-                });
+        listAll(folderRef).then((itemListResult) => {
+          itemListResult.items.forEach((itemRef) => {
+            getDownloadURL(ref(storage, itemRef.fullPath))
+              .then((myUrl) => {
+                console.log(category + ', ' + itemRef.fullPath)
+                CreateNewPhoto(category, myUrl)
               })
-        });
-      }).catch((error) => {
-    // Uh-oh, an error occurred!
-  });
+              .catch((error) => {})
+          })
+        })
+      })
+    })
+    .catch((error) => {
+      // Uh-oh, an error occurred!
+    })
 }
 
-//function to update the file variable with user input
-function handleChange(event) {
-  file = event.target.files[0]
-}
-const handleUpload = () => {
-
-  if (!file) {
-    alert("Please choose an image");
-
-
-  }
-
-  const storageRef = ref(storage, `/images/${file.name}`);
-
-  // Receives the storage reference and the file to upload.
-  const uploadTask = uploadBytesResumable(storageRef, file);
-
-  uploadTask.on(
-      "state_changed",
-      (err) => console.log(err),
-      () => {
-        // download url
-        getDownloadURL(uploadTask.snapshot.ref).then((url) => {
-          console.log(url);
-        });
-      });
-};
-
-function CreateNewPhoto(category, url)
-{
-  let newImg = new Image();
-  newImg.src = url;
-  newImg.alt = "Image not found";
-  newImg.className = category;
-  document.getElementById('galleryID').appendChild(newImg);
+function CreateNewPhoto(category, url) {
+  let newImg = new Image()
+  newImg.src = url
+  newImg.alt = 'Image not found'
+  newImg.className = category
+  document.getElementById('galleryID').appendChild(newImg)
 }
 
 //Might need the commented code below in the future, ask Paul before deleting please
@@ -99,48 +71,45 @@ async function pageToken(){
 }
 */
 
-function CreateInput(category){
-  let input = document.createElement("input");
-  input.type = "checkbox";
-  input.id = category;
-  input.name = category;
-  input.className = "input";
-  input.onclick = function(){
-    checkAll();
+function CreateInput(category) {
+  let input = document.createElement('input')
+  input.type = 'checkbox'
+  input.id = category
+  input.name = category
+  input.className = 'input'
+  input.onclick = function () {
+    checkAll()
   }
 
-  let label = document.createElement("label");
-  label.htmlFor = category;
-  label.appendChild(document.createTextNode(" " + category + " | "));
-  document.getElementById("inputs").appendChild(input);
-  document.getElementById("inputs").appendChild(label);
+  let label = document.createElement('label')
+  label.htmlFor = category
+  label.appendChild(document.createTextNode(' ' + category + ' | '))
+  document.getElementById('inputs').appendChild(input)
+  document.getElementById('inputs').appendChild(label)
 }
-function checkAll(){
-  let container = document.getElementsByClassName('input');
-  let images = document.getElementsByClassName('photo');
-  let showAll = true;
-  for (let i=0; i<container.length; i++){
-    if (container[i].checked){
-      showAll = false;
-    }
-
-  }
-  if (showAll){
-    for (let i=0; i<images.length; i++){
-      images[i].style.display = "flex";
+function checkAll() {
+  let container = document.getElementsByClassName('input')
+  let images = document.getElementsByClassName('photo')
+  let showAll = true
+  for (let i = 0; i < container.length; i++) {
+    if (container[i].checked) {
+      showAll = false
     }
   }
-  else{
-    for (let i=0; i<container.length; i++){
-      let pics = document.getElementsByClassName(container[i].name);
-      if (container[i].checked){
-        for (let j=0; j<pics.length; j++){
-          pics[j].style.display = "flex";
+  if (showAll) {
+    for (let i = 0; i < images.length; i++) {
+      images[i].style.display = 'flex'
+    }
+  } else {
+    for (let i = 0; i < container.length; i++) {
+      let pics = document.getElementsByClassName(container[i].name)
+      if (container[i].checked) {
+        for (let j = 0; j < pics.length; j++) {
+          pics[j].style.display = 'flex'
         }
-      }
-      else{
-        for (let j=0; j<pics.length; j++){
-          pics[j].style.display = "none";
+      } else {
+        for (let j = 0; j < pics.length; j++) {
+          pics[j].style.display = 'none'
         }
       }
     }
@@ -148,24 +117,129 @@ function checkAll(){
 }
 
 const GalleryPage = () => {
+  const [category, setCategory] = useState('/images/')
+  const [file, setFile] = useState(null)
+  function handleChange(event) {
+    setCategory(event.target.value)
+  }
+  const handleUpload = () => {
+    if (!file) {
+      alert('Please choose an image')
+    }
+
+    const storageRef = ref(storage, `${category}${file.name}`)
+
+    // Receives the storage reference and the file to upload.
+    const uploadTask = uploadBytesResumable(storageRef, file)
+
+    uploadTask.on(
+      'state_changed',
+      (err) => console.log(err),
+      () => {
+        // download url
+        getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+          console.log(url)
+        })
+      }
+    )
+    document.getElementById('file').value = null
+    setCategory('/images/')
+  }
   return (
-      <div style={{
+    <div
+      style={{
         minHeight: '100vh',
-        backgroundColor: '#333366' }}> {/* Solid color background */}
-        <h1 className='text-center text-3xl font-bold py-8' style={{
+        backgroundColor: '#003153',
+      }}
+    >
+      {' '}
+      {/* Solid color background */}
+      <h1
+        className="py-8 text-center text-3xl font-bold"
+        style={{
           color: '#FFFFFF',
           fontSize: '40px', // font larger
           fontWeight: 'bold', // font bold
           textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)', // shadow for depth
-        }}>Gallery</h1> {/*Start of body of Gallery, center text, text size, bold text */}
-        <div className={"grid-rows-4"}>
-          <div className={"grid-rows-4"}>
-            <h1 id={"inputs"} className='text-left text-3xl font-bold py-8' style={{color: '#FFFFFF'}}><span style={{marginRight: '16px'}}></span></h1>
-            <ul id={'galleryID'} className={"flex-container"}></ul>
-          </div>
-          <input type="file" onChange={handleChange} accept="/image/*"/>
-          <button onClick={handleUpload}>Upload</button>
+        }}
+      >
+        Gallery
+      </h1>{' '}
+      {/*Start of body of Gallery, center text, text size, bold text */}
+      <div className={'grid-rows-4'}>
+        <div className={'grid-rows-4'}>
+          <h1 id={'inputs'} className="py-8 text-left text-3xl font-bold" style={{color: '#FFFFFF'}}>
+            <span style={{marginRight: '16px'}}></span>
+          </h1>
+          <ul id={'galleryID'} className={'flex-container'}></ul>
         </div>
+      </div>
+      <div className="rounded-lg w-fit flex-col space-y-4 border-4 border-white">
+        <h1 className="items-center text-center text-lg font-bold text-white">Add Photo to Gallery</h1>
+
+        <form className="flex-col items-start space-y-1 text-white" onSubmit>
+          <div>
+            <h1 className="text-md items-center text-left text-white">Choose Category</h1>
+
+            <input
+              type="radio"
+              name="category"
+              value="/images/"
+              id="images"
+              checked={category === '/images/'}
+              onChange={handleChange}
+            />
+            <label className="text-md text-white">None</label>
+            <input
+              type="radio"
+              name="category"
+              value="/images/firearm_accessories/"
+              id="firearm_accessories"
+              checked={category === '/images/firearm_accessories/'}
+              onChange={handleChange}
+            />
+            <label className="text-md text-white">Firearm Accessories</label>
+            <input
+              type="radio"
+              name="category"
+              value="/images/firearms/"
+              id="firearms"
+              checked={category === '/images/firearms/'}
+              onChange={handleChange}
+            />
+            <label className="text-md text-white">Firearms</label>
+            <input
+              type="radio"
+              name="category"
+              value="/images/thermos/"
+              id="thermos"
+              checked={category === '/images/thermos/'}
+              onChange={handleChange}
+            />
+            <label className="text-md text-white">Thermos</label>
+          </div>
+          <div className="flex w-full ">
+            <input
+              className=""
+              type="file"
+              name="file"
+              id="file"
+              onChange={(event) => setFile(event.target.files[0])}
+              accept="/image/*"
+            />
+          </div>
+          <div>
+            <button
+              type="button"
+              className="rounded-lg bg-black px-2.5 py-1.5 text-center text-sm
+                font-semibold text-white"
+              onClick={handleUpload}
+            >
+              Upload
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
