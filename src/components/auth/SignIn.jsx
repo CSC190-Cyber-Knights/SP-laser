2b93d64b27f562513002fcc4b22bf6d2b3764cd4
bob dylend <jamesayers00@gmail.com>
2024-04-09 19:37:35 -0700

    signin page added forget password, added some basic styling, signup page(not needed)

--- src/components/auth/SignIn.jsx
+++ src/components/auth/SignIn.jsx
@@ -44,35 +44,73 @@ export const SignIn = () => {
   }
 
   return (
-    <div className="flex min-h-screen flex-col items-center">
+    <div className="flex min-h-screen flex-col items-center" style={{backgroundColor: '#003153'}}>
       <div>
-        <h1 className="py-8 text-center text-3xl font-bold">Admin Sign In</h1>
+        <h1 className="py-8 text-center text-3xl font-bold" style={{color: '#FFFFFF'}}>
+          Admin Sign In
+        </h1>
         <form onSubmit={SignIn}>
           <div>
-            <input
-              type="email"
-              placeholder="Enter your email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            ></input>
+            <div className="flex w-full flex-col">
+              <label
+                htmlFor="email"
+                className="text-neutral-600"
+                style={{
+                  fontWeight: 'bold',
+                  color: '#FFFFFF',
+                  textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)',
+                }}
+              >
+                Email Address
+              </label>
+              <input
+                className="rounded-lg h-12 border-slate-200 bg-neutral-100 p-2 shadow"
+                type="email"
+                placeholder="Enter your email"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              ></input>
+            </div>
+            <div className="flex w-full flex-col">
+              <label
+                htmlFor="email"
+                className="text-neutral-600"
+                style={{
+                  fontWeight: 'bold',
+                  color: '#FFFFFF',
+                  textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)',
+                }}
+              >
+                Password
+              </label>
+              <input
+                className="rounded-lg h-12 border-slate-300 bg-neutral-100 p-2 shadow"
+                type="password"
+                placeholder="Enter your password"
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              ></input>
+            </div>
           </div>
-
-          <div>
-            <input
-              type="password"
-              placeholder="Enter your password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            ></input>
-          </div>
-
-          <div>
-            <button type="submit">Sign In</button>
+          <div className="flex w-full flex-col">
+            <button className="rounded-lg w-full bg-neutral-800 py-1 text-lg font-light text-neutral-300" type="submit">
+              Sign Up
+            </button>
           </div>
         </form>
       </div>
 
-      <p onClick={handlePasswordReset} className="forgot-password">
+      <p
+        onClick={handlePasswordReset}
+        className="forgot-password"
+        style={{
+          fontWeight: 'bold',
+          color: '#FFFFFF',
+          textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)',
+        }}
+      >
         Forgot Password?
       </p>
 
@@ -82,6 +120,7 @@ export const SignIn = () => {
           userSignIn()
         }}
       />
+
       <button
         onClick={() => {
           userSignOut()
--- src/components/auth/SignUp.jsx
+++ src/components/auth/SignUp.jsx
@@ -1,22 +1,31 @@
 import {GoogleButton} from 'react-google-button'
 import {userSignIn, userSignOut} from '../../services/firebase.js'
-import {useState} from 'react'
-import {createUserWithEmailAndPassword, getAuth} from 'firebase/auth'
+import {useEffect, useState} from 'react'
+import {createUserWithEmailAndPassword, getAuth, sendEmailVerification} from 'firebase/auth'
+import emailjs from '@emailjs/browser'
 
 {
   /*SignUp features a google click login if already logged into google, and a signout*/
 }
 
+//EmailJS credentials
+const EmailServiceID = 'service_zadrexa'
+const EmailTemplateID = 'template_wi09qe3'
+const EmailPublicKey = 'Io04H3uOQN5v-GZbf'
+
 export const SignUp = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
   const auth = getAuth()
-  const signUp = (e) => {
+  const signUp = async (e) => {
     e.preventDefault()
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
+    await createUserWithEmailAndPassword(auth, email, password)
+      .then(async (userCredential) => {
         const user = userCredential.user
         console.log(userCredential)
+        await sendEmailVerification(user)
+        console.log('Success')
       })
       .catch((error) => {
         const errorCode = error.code
@@ -24,51 +33,107 @@ export const SignUp = () => {
         console.log(error)
       })
   }
+
+  useEffect(() => {
+    auth.onAuthStateChanged((userCredential) => {
+      const {email, emailVerified} = userCredential
+      setUser({email, emailVerified})
+    })
+  })
+
   return (
-    <div className="flex min-h-screen flex-col items-center">
+    <div className="flex min-h-screen flex-col items-center" style={{backgroundColor: '#003153'}}>
       <div>
-        <h1 className="py-8 text-center text-3xl font-bold">Admin Sign Up</h1>
+        <h1 className="py-8 text-center text-3xl font-bold" style={{color: '#FFFFFF'}}>
+          Admin Sign Up
+        </h1>
         <form onSubmit={signUp}>
           <div>
-            <input
-              type="email"
-              placeholder="Enter your email"
-              required
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            ></input>
+            <div className="flex w-full flex-col">
+              <label
+                htmlFor="email"
+                className="text-neutral-600"
+                style={{
+                  fontWeight: 'bold',
+                  color: '#FFFFFF',
+                  textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)',
+                }}
+              >
+                Email Address
+              </label>
+              <input
+                className="rounded-lg h-12 border-slate-200 bg-neutral-100 p-2 shadow"
+                type="email"
+                placeholder="Enter your email"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              ></input>
+            </div>
+            <div className="flex w-full flex-col">
+              <label
+                htmlFor="email"
+                className="text-neutral-600"
+                style={{
+                  fontWeight: 'bold',
+                  color: '#FFFFFF',
+                  textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)',
+                }}
+              >
+                Password
+              </label>
+              <input
+                className="rounded-lg h-12 border-slate-300 bg-neutral-100 p-2 shadow"
+                type="password"
+                placeholder="Enter your password"
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              ></input>
+            </div>
           </div>
-          <div>
-            <input
-              type="password"
-              placeholder="Enter your password"
-              required
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            ></input>
-          </div>
-          <div>
-            <button type="submit">Sign Up</button>
+          <div className="flex w-full flex-col">
+            <button className="rounded-lg w-full bg-neutral-800 py-1 text-lg font-light text-neutral-300" type="submit">
+              Sign Up
+            </button>
           </div>
         </form>
       </div>
 
-      {/*allow google account log in*/}
-      <GoogleButton
-        onClick={() => {
-          userSignIn()
-        }}
-      />
-      <button
-        onClick={() => {
-          userSignOut()
-        }}
-      >
-        Sign Out
-      </button>
+      <div>
+        {/*allow google account log in*/}
+        <GoogleButton
+          onClick={() => {
+            userSignIn()
+          }}
+        />
+      </div>
+
+      <div>
+        <button
+          className=""
+          style={{color: '#FFFFFF'}}
+          onClick={() => {
+            userSignOut()
+          }}
+        >
+          Sign Out
+        </button>
+      </div>
     </div>
   )
 }
+/* used to check email verifcation
+{user && (
+      <div>
+      <p>{user?.email}
+      </p>
+      <p>{user?.emailVerified ? "True" : "False"}
+      </p>
+      </div>
+    )}
+/*/
+
 /*
 //const signInButton = document.getElementById("signInButton")
 //const signOutButton = document.getElementById("signOutButton")
