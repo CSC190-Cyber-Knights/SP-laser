c0528bcb869859968a44eb9f7adb8a16bc290633
Anish Rajah <113150187+anishrajah@users.noreply.github.com>
2024-04-18 13:34:34 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,10 +15,11 @@ export default {
         imposter_red: '#FF0035',
         cad_grey: '#8B939C',
         eng_violet: '#59546C',
-        def_blue: '#38405F',
+        def_blue: '#C0C0C0',
         mat_grey: '#121212',
         rich_black: '#0E131F',
         noir_dark: '#e8e6e3',
+        deep_charcoal: '#333333',
       },
       backgroundImage: {
         'random-line': "url('./src/assets/line.svg')",
