06579045a392274391d18cba542ad50f7712571c
Anish Rajah <113150187+anishrajah@users.noreply.github.com>
2024-04-27 11:47:26 -0700

    Added a sign out button to the upload form
    The sign out button will trigger a page switch back to the sign in page

--- src/components/ui/UploadForm.jsx
+++ src/components/ui/UploadForm.jsx
@@ -5,6 +5,9 @@ import {saveTag} from '../../firebase/libs/upload.js'
 import TagInput from './tags/TagInput.jsx'
 import {Photo} from './media/Photo.jsx'
 import ListTags from './tags/ListTags.jsx'
+import {auth} from "../../services/firebase.js";
+import {useNavigate} from "react-router-dom";
+import {signOut} from "firebase/auth";
 
 export const UploadForm = () => {
   const [isLoading, setIsLoading] = useState(false)
@@ -14,7 +17,19 @@ export const UploadForm = () => {
   const [file, setFile] = useState(null)
   const [error, setError] = useState(null)
   const [preview, setPreview] = useState('')
+  const navigate = useNavigate()
 
+  const userSignOutWithNavigate = async () => {
+
+    signOut(auth)
+        .then(() => {
+          navigate('/signin')
+          alert('You have been signed out')
+        })
+        .catch((e) => {
+          console.error(e)
+        })
+  }
   useEffect(() => {
     getTags().then((fetchedTags) => {
       setTags(fetchedTags)
@@ -68,34 +83,41 @@ export const UploadForm = () => {
   }
 
   return (
-    <div className="flex w-full flex-col items-center gap-4">
-      <h2 className="text-2xl font-bold dark:text-egg">Upload Photo</h2>
-      {preview && (
-        <div className={'w-52'}>
-          <Photo src={preview} />
+      <div className="flex w-full flex-col items-center gap-4">
+        <h2 className="text-2xl font-bold dark:text-egg">Upload Photo</h2>
+        {preview && (
+            <div className={'w-52'}>
+              <Photo src={preview}/>
+            </div>
+        )}
+        {tags.length > 0 && <ListTags tags={tags} selectedTag={selectedTag} onSelectTag={setSelectedTag}/>}
+        <div className="mb-3 w-96">
+          <input
+              className="focus:shadow-te-primary relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-neutral-700 dark:file:text-neutral-100 dark:focus:border-primary"
+              disabled={isLoading}
+              onChange={handleFileChange}
+              type="file"
+              id="formFile"
+          />
         </div>
-      )}
-      {tags.length > 0 && <ListTags tags={tags} selectedTag={selectedTag} onSelectTag={setSelectedTag} />}
-      <div className="mb-3 w-96">
-        <input
-          className="focus:shadow-te-primary relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-neutral-700 dark:file:text-neutral-100 dark:focus:border-primary"
-          disabled={isLoading}
-          onChange={handleFileChange}
-          type="file"
-          id="formFile"
-        />
-      </div>
 
-      <TagInput onAddTag={handleAddTag} />
-      <button
-        onClick={handleSubmit}
-        className={`rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 ${isLoading ? 'cursor-not-allowed opacity-50' : ''}`}
-        disabled={isLoading}
-      >
-        {isLoading ? 'Uploading...' : 'Upload Photo'}
-      </button>
-      {success && <p className="text-green-500">✅ Upload successful!</p>}
-      {error && <p className="text-red-500">{error}</p>}
-    </div>
+        <TagInput onAddTag={handleAddTag}/>
+        <button
+            onClick={handleSubmit}
+            className={`rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 ${isLoading ? 'cursor-not-allowed opacity-50' : ''}`}
+            disabled={isLoading}
+        >
+          {isLoading ? 'Uploading...' : 'Upload Photo'}
+        </button>
+        {success && <p className="text-green-500">✅ Upload successful!</p>}
+        {error && <p className="text-red-500">{error}</p>}
+        <button
+            onClick={() => {
+              userSignOutWithNavigate()
+            }}
+        >
+          Sign Out
+        </button>
+      </div>
   )
 }
