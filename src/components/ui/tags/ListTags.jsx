ee625bbaf41778130d495c487b5bdbe20a2a1218
Anish Rajah <113150187+anishrajah@users.noreply.github.com>
2024-04-28 16:44:28 -0700

    added id tags for both files
    selenium testing

--- src/components/ui/tags/ListTags.jsx
+++ src/components/ui/tags/ListTags.jsx
@@ -3,6 +3,7 @@ export function ListTags({tags, selectedTag, onSelectTag}) {
     <div className="wrap flex w-full items-center justify-center gap-4">
       {tags.map((tag, index) => (
         <button
+            id = "tagbtn"
           key={index}
           onClick={() => onSelectTag(tag.name)}
           className={`h-fit rounded px-2 py-1 ${selectedTag === tag.name ? 'bg-blue-500 text-white' : 'bg-gray-200 text-black'}`}
--- src/components/ui/tags/TagInput.jsx
+++ src/components/ui/tags/TagInput.jsx
@@ -15,6 +15,7 @@ export default function TagInput({onAddTag}) {
       >
         <div className="max-w-sm space-y-3">
           <input
+              id = "tagtext"
             type="text"
             className="block w-full rounded-l-full border-gray-200 px-4 py-3 text-sm focus:border-blue-500 focus:ring-blue-500 disabled:pointer-events-none disabled:opacity-50  dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
             value={tag}
@@ -23,6 +24,7 @@ export default function TagInput({onAddTag}) {
           />
         </div>
         <button
+            id = "submittags"
           type="submit"
           className="rounded-r-full bg-imposter_red px-6 py-3 text-sm font-bold text-white hover:bg-blue-700"
         >
