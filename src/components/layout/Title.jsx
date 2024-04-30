a47b1e186b8dff4a266aa939f1f56d585e7593c6
chris <actuallychrismendez@gmail.com>
2024-04-29 21:44:54 -0700

    changed title stylwe

--- src/components/layout/Title.jsx
+++ src/components/layout/Title.jsx
@@ -3,11 +3,7 @@ import {Fragment} from 'react'
 const Title = ({hero, subHero, Icon}) => {
   return (
     <Fragment>
-      <div
-        className={
-          'h-18 my-4 flex w-full flex-col items-center gap-3 pt-6 text-center leading-tight dark:text-neutral-100'
-        }
-      >
+      <div className={'h-18 my-4 flex w-full flex-col items-center gap-3 pt-6 text-center leading-tight'}>
         <div className={'flex w-fit flex-col sm:w-fit sm:flex-row sm:items-center sm:text-center'}>
           {Icon && <Icon className={'mr-2 hidden text-5xl sm:block'} />}
           <h1 className={'h-full w-full text-6xl font-semibold capitalize sm:text-5xl'}>{hero}</h1>
